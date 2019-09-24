7d655b81ea2f7a275906ec201f5a96d34e4f2eed
chjjh0 <chjjh0@gmail.com>
2019-09-24 14:19:47 +0900

    intro

---
+++ js/changemenu.js
@@ -0,0 +1,12 @@
+document.documentElement.className = "js";
+    var supportsCssVars = function() {
+        var e,t=document.createElement("style");
+        return (
+            t.innerHTML = "root: { --tmp-var: bold; }",
+            document.head.appendChild(t),
+            e = !!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),
+            t.parentNode.removeChild(t),
+            e
+        )
+        };
+supportsCssVars() || alert("I'm sorry, Please Move To https://github.com/chjjh0");
